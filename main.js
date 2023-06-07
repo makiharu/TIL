ef124229aa80395c4f76bf9c91a3a644b7622faa
makiharu <kitakazetoshirokuma@gmail.com>
2023-06-08 00:03:08 +0900

    day1-constructor

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-window.alert('test');
+class Lunch {
+  constructor(main, dessert, drink) {
+    this.main = main;
+    this.dessert = dessert;
+    this.drink = drink;
+  }
+}
+
+let lunch1 = new Lunch('bread', 'cake', 'cola');
+console.log(lunch1.main);
+
+let lunch2 = new Lunch('rice', 'prin', 'soda');
+console.log(lunch2.dessert);
+
+let lunch3 = new Lunch('bread', 'fruit', 'greentea');
+console.log(lunch3.main);
+console.log(lunch3.drink);
