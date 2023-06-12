5defcd4a11beca6d7695184ee746bdafe0c9bb20
makiharu <kitakazetoshirokuma@gmail.com>
2023-06-12 19:42:13 +0700

    5-2

---
+++ code5-2.js
@@ -0,0 +1,28 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+//   console.log(lines[0]);
+    const n = Number(lines[0]);
+    const arr = lines[1].split(' ').map(Number);
+    const k = Number(lines[2]);
+
+    let flg = false;
+    for(let i=0; i < arr.length; i++) {
+        if(arr[i] === k) {
+            console.log(i+1);
+            flg = true;
+        }
+        if(flg === true) break;
+    }
+    if(flg === false) console.log(0);
+});
