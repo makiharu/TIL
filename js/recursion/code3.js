4991df9b6867cb7251b0d78704708ddee5423381
makiharu <kitakazetoshirokuma@gmail.com>
2023-08-03 23:23:28 +0900

    ハノイの塔

---
+++ js/recursion/code3.js
@@ -0,0 +1,10 @@
+function towerOfHanoi(discs){
+    if(discs <= 1) return 1;
+
+    return towerOfHanoi(discs-1) + 1 + towerOfHanoi(discs-1);
+}
+
+console.log(towerOfHanoi(1));
+console.log(towerOfHanoi(3));
+console.log(towerOfHanoi(5));
+console.log(towerOfHanoi(10));
