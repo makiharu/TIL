c90efeb8b4b57d6d7357c24c351df2f9f347269a
makiharu <kitakazetoshirokuma@gmail.com>
2023-09-08 23:59:46 +0900

    test

--- js/21_game/card.js
+++ js/21_game/card.js
@@ -1,21 +1,21 @@
-// まずは1枚分のカードを表すクラスCardを生成しましょう。
-// 記号(♣, ♦, ♥, ♠の内1つ)・値（A,2,~,Kの内1つ）・数値（0~12の内1つ）をインスタンス化させるコンストラクタと、それらの情報を返すメソッドgetCardStringを作成してください。
-// ♥8を例にコンソールに出力してください。
+// Math.random --> 0以上1未満のランダムな数
+console.log(Math.random());
 
-// ここから記述してください。
-class Card{
-    constructor(suit, value, intValue) {
-        this.suit = suit;
-        this.value = value;
-        this.intValue = intValue;
-    }
+// 0以上10未満のランダムな数
+console.log(Math.random() * 10);
 
-    getCardString() {
-        return this.suit + this.value + "(" + this.intValue +")";
-    }
+// 0以上10未満のランダムな整数
+console.log(Math.floor(Math.random() * 10));
 
+let intArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(intArr);
+
+for (let i = intArr.length - 1; i >= 0 ; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+
+    let temp = intArr[i];
+    intArr[i] = intArr[j];
+    intArr[j] = temp;
 }
 
-let card1 = new Card("♦︎","A",1);
-console.log(card1);
-console.log(card1.getCardString());
+console.log(intArr);
